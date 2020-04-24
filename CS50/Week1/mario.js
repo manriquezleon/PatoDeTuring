640119efe46e5ac95039bbd1315947be14b42839
SoyMartin <13058234+Alquimista3301@users.noreply.github.com>
2020-04-24 14:51:17 -0500

    Update mario.js

--- CS50/Week1/mario.js
+++ CS50/Week1/mario.js
@@ -1,1 +1,22 @@
+function mario() {
+    var num=0;
+    do{
+        var num = prompt('Heigth: ');
+    } while (num<=0 & num>8);
 
+    var count = num;
+    var temp = 0;
+    for(var i=0;i<num;i++){
+        count--;
+        temp++;
+        var str = "";
+        for (var j = 0; j < count; j++)
+        {
+            str=str+" ";
+        }
+        for(var j = 0; j < temp; j++){
+            str=str+"*";
+        }
+        console.log(str);
+    }
+}
